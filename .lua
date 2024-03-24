c586e327df07d1ff6ff87f9caf4334db705dd41b
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-03-24 20:52:42 +0900

    Update .lua

--- .lua
+++ .lua
@@ -1,5 +1,7 @@
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sidhsksjsjsh/VAPE-UI-MODDED/main/.lua"))()
 local wndw = lib:Window("VIP Turtle Hub V4")
+local workspace = game:GetService("Workspace")
+local self = game.Players.LocalPlayer
 local T1 = wndw:Tab("Main")
 local T3 = wndw:Tab("Easter Event")
 
@@ -14,10 +16,18 @@ local const = {
     toggle = false
   },
   easter = false,
-  reb = false
+  reb = false,
+  near = true
 }
 
 --lib:AddTable(,{})
+
+local function child(path,funct)
+  for i,v in pairs(path:GetChildren()) do
+    funct(v)
+  end
+end
+
  T1:Toggle("Auto train",false,function(value)
     const.train = value
     while wait() do
@@ -65,6 +75,27 @@ T1:Toggle("Auto rebirth",false,function(value)
     const.reb = value
     while wait() do
       if const.reb == false then break end
-      
+      game:GetService("ReplicatedStorage")["BridgeNet"]["dataRemoteEvent"]:FireServer({"R","\4"})
+    end
+  end)
+
+T3:Toggle("Use nearest system [ 25 Distance ]",true,function(value)
+    const.near = value
+end)
+
+--{{"Hit",v.Name},"\16","\11"}
+T3:Toggle("Auto hit all eggs",false,function(value)
+    const.easter = value
+    while wait() do
+      if const.easter == false then break end
+      child(workspace.Items.Easter.ClientEasterEggs,function(v)
+          if const.near == true then
+            if (self.Character.HumanoidRootPart.Position - v.Position).Magnitude < 25 then
+              game:GetService("ReplicatedStorage")["BridgeNet"]["dataRemoteEvent"]:FireServer({{"Hit",v.Name},"\16","\11"})
+            end
+          else
+            game:GetService("ReplicatedStorage")["BridgeNet"]["dataRemoteEvent"]:FireServer({{"Hit",v.Name},"\16","\11"})
+          end
+      end)
     end
   end)
